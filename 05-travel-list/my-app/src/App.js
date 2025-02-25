5320908d3b4009fb4d3d17d061f06ad91484ccfb
iceiceice <597952291@qq.com>
2025-02-25 19:20:08 +0800

    update

--- 05-travel-list/my-app/src/App.js
+++ 05-travel-list/my-app/src/App.js
@@ -7,11 +7,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <From />
-      <PackingList />
+      <From onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -22,13 +26,18 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function From() {
+function From({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
     setDescription("");
     setQuantity(1);
   }
@@ -48,11 +57,11 @@ function From() {
   </form>
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
