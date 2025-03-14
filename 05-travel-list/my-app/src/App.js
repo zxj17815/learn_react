1cc2ce4c2b3095aff0976263816534292112622b
iceiceice <597952291@qq.com>
2025-03-14 16:08:22 +0800

    update

--- 05-travel-list/my-app/src/App.js
+++ 05-travel-list/my-app/src/App.js
@@ -16,12 +16,18 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item))
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <From onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handDeleteItem} />
-      <Stats />
+      <PackingList items={items} onDeleteItem={handDeleteItem} onToggleItem={handleToggleItem} />
+      <Stats items={items} />
     </div>
   );
 }
@@ -64,26 +70,33 @@ function From({ onAddItems }) {
   </form>
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
     </div>
   )
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) return <p className="stats"><em>Start adding some items to your packing list 🚀</em></p>
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return <footer className="stats">
-    <em> You have x items on your list, and you already packed x items</em>
+    <em> {percentage === 100 ? "You got everything! Ready to go ✈️" :
+      `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)items`}
+    </em>
   </footer>
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return <li>
+    <input type="checkbox" checked={item.packed} onChange={() => onToggleItem(item.id)} />
     <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
     {/* <button>{item.packed ? "✅" : "❌"}</button> */}
     <button onClick={() => onDeleteItem(item.id)}>❌</button>
