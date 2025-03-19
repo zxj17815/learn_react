079bea0f77c582ac3f6c9437a5b437604eeb7368
iceiceice <597952291@qq.com>
2025-03-19 16:59:58 +0800

    update

---
+++ 05-travel-list/my-app/src/components/Item.js
@@ -0,0 +1,8 @@
+export default function Item({ item, onDeleteItem, onToggleItem }) {
+    return <li>
+        <input type="checkbox" checked={item.packed} onChange={() => onToggleItem(item.id)} />
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
+        {/* <button>{item.packed ? "✅" : "❌"}</button> */}
+        <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>;
+}
