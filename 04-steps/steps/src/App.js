6df6b115b07185231cd79491f6d90f8c48ec1a3a
iceiceice <597952291@qq.com>
2025-03-27 16:26:15 +0800

    update

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -35,7 +35,7 @@ export default function App() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">Step {step}: {messages[step - 1]}</p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious} >Previous</button>
@@ -44,4 +44,14 @@ export default function App() {
         </div>)}
     </>
   );
+}
+
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> Step {step}</h3>
+      {children}
+    </div>
+  )
 }
